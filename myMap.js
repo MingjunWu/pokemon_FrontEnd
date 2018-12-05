cc4bb4dc61bff0c683d1cd1b6151b3e625a12666
MingjunWu <wmingjun94@gmail.com>
2018-12-05 17:35:36 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,10 +1,12 @@
 var map;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
-}
+
 
 var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
 var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
     anchor: new Microsoft.Maps.Point(12, 39) });
 map.entities.push(pushpin);
+    
+}
 
